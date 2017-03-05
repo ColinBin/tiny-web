19bce903ef04f0fda78680a61e9ebddd29f2241d
Colin <1413278796@qq.com>
2017-03-05 21:35:12 +0800

    Support HEAD. Reap children with handler.

--- tiny_web/cgi-source/adder.c
+++ tiny_web/cgi-source/adder.c
@@ -2,28 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAXBUF 4096
+#define MAXLINE 1024
 int main(int argc, char **argv){
 	int a, b, sum;
 	char *cgiargs, *ptr;
 	char buf[MAXBUF];
+	char *method_head;
 	cgiargs = getenv("QUERY_STRING");
-	if(cgiargs && (ptr = strchr(cgiargs, '&'))) {
-	 	*ptr = '\0';
-		a = atoi(cgiargs);
-		b = atoi(ptr + 1);
-		sum = a + b;
-	} 
-	else 
-		sum = 0;
 	
-	sprintf(buf, "<html><title>Tiny Web Server</title>\r\n");
-	sprintf(buf, "%s<body><h1>The result is: %d</h1>\r\n", buf, sum);
-	sprintf(buf, "%s<p>Thanks for using adder!\r\nHave a nice day</p></body></html>", buf);
+	if(cgiargs && (ptr = strchr(cgiargs, '&')) && strstr(cgiargs, "first=") && strstr(cgiargs, "second=")) {
+		// display the result
+		*ptr = '\0';
+		a = atoi(cgiargs + strlen("first="));
+		b = atoi(ptr + 1 + strlen("second="));
+		sum = a + b;
+
+		sprintf(buf, "<html><title>Tiny Web Server</title>\r\n");
+		sprintf(buf, "%s<body><h1>The result is: %d</h1>\r\n", buf, sum);
+		sprintf(buf, "%s<p>Thanks for using adder!\r\nHave a nice day</p></body></html>", buf);
+	}
+	else {
+		// display the form
+		sprintf(buf, "<html><form action=\"adder\" method=\"get\">\r\n");
+		sprintf(buf, "%s<p>#1 number: <input type=\"text\" name=\"first\"/></p>\r\n", buf);
+		sprintf(buf, "%s<p>#2 number: <input type=\"text\" name=\"second\"/></p>\r\n", buf);
+		sprintf(buf, "%s<input type=\"submit\" value=\"submit\" /></form></html>\r\n", buf);
+	}
 
 	printf("Content-type: text/html\r\n");
 	printf("Content-length: %lu\r\n", strlen(buf));
 	printf("Server: Tiny Web Server\r\n\r\n");
 
+	method_head = getenv("METHOD_HEAD");
+	if(!strcmp(method_head, "TRUE")) {
+		return 0;
+	}
 	printf("%s", buf);
 
 	return 0;
--- tiny_web/tiny_web.c
+++ tiny_web/tiny_web.c
@@ -1,5 +1,8 @@
 #include "../robust_io/rio.h"
 #include "../socket_pack/socket_pack.h"
+#include <signal.h> 	/* for capturing SIGCHLD*/
+#include <stdio.h>
+#include <unistd.h>
 
 #define MAXLINE 1024
 #define MAXBUF 4096
@@ -10,17 +13,32 @@ void serve(int connfd);
 void client_error(int connfd, char *cause, char *error_number, char *short_msg, char *long_msg);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int connfd, char *filename, int filesize);
-void serve_dynamic(int connfd, char *filename, char *cgiargs);
+void serve_static(int connfd, char *filename, int filesize, int method_head);
+void serve_dynamic(int connfd, char *filename, char *cgiargs, int method_head);
 void check_filetype(char *filename, char *filetype);
+int check_method(char *method, char *standard_method);				// return 1 if method is standard_method, 0 otherwise
 
+void handler(int sig) {
+	pid_t pid;
+	while((pid = waitpid(-1, NULL, 0)) > 0) {  }
+	if(errno != ECHILD) {
+		fprintf(stderr, "Waitpid Error\n");
+		exit(3);
+	}
+}
 int main(int argc, char **argv) {
-	
+
 	int server_fd;
 	int port;
 	int connfd;
 	socklen_t clientaddr_len;
 	struct sockaddr_in clientaddr;
+
+	/* reaps cgi children with a signal handler */
+	if(signal(SIGCHLD, handler) == SIG_ERR) {
+		fprintf(stderr, "Signal Error\n");
+		exit(3);
+	}
 
 	if(argc != 2) {
 		fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -48,17 +66,25 @@ void serve(int connfd) {
 	char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
 	char cgiargs[MAXLINE], filename[MAXLINE];
 	int is_static;
+	int method_head;
 	struct stat filestat;
 	rio_readinitb(&rio, connfd);
 
 	rio_readlineb(&rio, buf, MAXLINE);
 	sscanf(buf, "%s %s %s", method, uri, version);
-	if(strcasecmp(method, "GET")) {
-		client_error(connfd, method, "501", "Not Implemented", "Tiny web does not support this method(, yet)");	
+
+	if(check_method(method, "GET")) {
+		method_head = 0;
+	} else if(check_method(method, "HEAD")) {
+		method_head = 1;
+	} else {
+		client_error(connfd, method, "501", "Not Implemented", "Tiny web does not support this method(, yet)");
 		return ;
 	}
-	read_requesthdrs(&rio);	
-	
+
+	printf("%s", buf);		/* print request line */
+	read_requesthdrs(&rio);
+
 	is_static = parse_uri(uri, filename, cgiargs);
 	if(stat(filename, &filestat) < 0) {
 		client_error(connfd, filename, "404", "Not found", "Tiny cannot find this file");
@@ -68,14 +94,14 @@ void serve(int connfd) {
 		if(!(S_ISREG(filestat.st_mode)) || !(S_IRUSR & filestat.st_mode)){
 			client_error(connfd, filename, "403", "Forbidden", "Tiny cannot open this file");
 			return ;
-		}	
-		serve_static(connfd, filename, filestat.st_size);
+		}
+		serve_static(connfd, filename, filestat.st_size, method_head);
 	} else {
 		if(!(S_ISREG(filestat.st_mode)) || !(S_IXUSR & filestat.st_mode)) {
 			client_error(connfd, filename, "403", "Forbidden", "Tiny cannot execute this file");
 			return;
 		}
-		serve_dynamic(connfd, filename, cgiargs);
+		serve_dynamic(connfd, filename, cgiargs, method_head);
 	}
 
 }
@@ -99,7 +125,7 @@ void read_requesthdrs(rio_t *rp) {
 	char buf[MAXLINE];
 	rio_readlineb(rp, buf, MAXLINE);
 	while(strcmp(buf, "\r\n")) {
-		printf("%s\n", buf);
+		printf("%s", buf);
 		rio_readlineb(rp, buf, MAXLINE);
 	}
 }
@@ -125,12 +151,12 @@ int parse_uri(char *uri, char *filename, char *cgiargs) {
 		}
 		strcpy(cgiargs, "");
 		return 1;
-	}	
+	}
 }
 
-void serve_static(int connfd, char *filename, int filesize){
+void serve_static(int connfd, char *filename, int filesize, int method_head){
 	char buf[MAXLINE];
-	char filetype[MAXLINE];			
+	char filetype[MAXLINE];
 	check_filetype(filename, filetype);
 
 	sprintf(buf, "HTTP/1.1 200 OK\r\n");
@@ -141,7 +167,9 @@ void serve_static(int connfd, char *filename, int filesize){
 	rio_writen(connfd, buf, strlen(buf));
 	sprintf(buf, "Content-length: %d\r\n\r\n", filesize);
 	rio_writen(connfd, buf, strlen(buf));
-
+	if(method_head) {
+		return;
+	}
 	int filefd = open(filename, O_RDONLY, 0);
 	/* use memory mapping to copy file content to memory */
 	char *mapped_ptr;
@@ -163,7 +191,13 @@ void check_filetype(char *filename, char *filetype){
 	}
 }
 
-void serve_dynamic(int connfd, char *filename, char *cgiargs) {
+int check_method(char *method, char *standard_method){
+	if(strcasecmp(method, standard_method))
+		return 0;
+	else
+		return 1;
+}
+void serve_dynamic(int connfd, char *filename, char *cgiargs, int method_head) {
 	char buf[MAXLINE];
 	char *emptylist[] = { NULL };
 	sprintf(buf, "HTTP/1.1 200 OK\r\n");
@@ -172,10 +206,13 @@ void serve_dynamic(int connfd, char *filename, char *cgiargs) {
 	rio_writen(connfd, buf, strlen(buf));
 	if(fork() == 0) {
 		/* should set all environment variables */
+		if(method_head){
+			setenv("METHOD_HEAD", "TRUE", 1);
+		} else{
+			setenv("METHOD_HEAD", "FALSE", 1);
+		}
 		setenv("QUERY_STRING", cgiargs, 1);
 		dup2(connfd, STDOUT_FILENO);
 		execve(filename, emptylist, environ);
 	}
-	
-	wait(NULL);
 }
